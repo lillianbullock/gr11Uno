17e432b4c296c23a298ffc93516fd22af258837e
L. Brooke Bullock <lillianbullock@gmail.com>
2017-10-11 13:41:38 -0600

    Create BuildDeck.h

---
+++ BuildDeck.h
@@ -0,0 +1,17 @@
+//
+//  build_deck.h
+//  final
+//
+//  Created by Brooke Bullock on 2014-12-16.
+//  Copyright (c) 2014 Brooke Bullock. All rights reserved.
+//
+
+#ifndef __final__BuildDeck__
+#define __final__BuildDeck__
+
+#include <vector>
+#include "subroutines.h"
+
+vector <card> BuildDeck (void);
+
+#endif /* defined(__final__BuildDeck__) */
